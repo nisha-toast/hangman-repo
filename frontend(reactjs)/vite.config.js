8fd05f1e11b92402bb89dc80293c347754b4f269
nrani01 <pnisharani1@gmail.com>
2025-02-19 20:36:18 +0800

    Adding homepage url to package.json and base to vite.config.js

--- frontend(reactjs)/vite.config.js
+++ frontend(reactjs)/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base:'/hangman-repo',
   server:{
     proxy: {
       '/api': 'http://localhost:8091',
